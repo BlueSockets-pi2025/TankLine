b53f97061717e0cd565b5451da034340ba4483e2
Tiuxi <thigal.pierre@gmail.com>
2025-02-28 14:07:23 +0100

    basic bullet prefab

---
+++ TankLine-Client/Assets/Scripts/Tanks/Bullet.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public int nbRebounds = 1;
+    public Vector3 direction;
+    public float bulletSpeed = 5;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
