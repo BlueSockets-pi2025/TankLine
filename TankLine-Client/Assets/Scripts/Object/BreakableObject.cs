59a6874f8efa78d8ad703cec0f6b6aa5c8dd2314
starplaye <39805888+starplaye@users.noreply.github.com>
2025-05-05 13:06:43 +0200

    modif prefab

--- TankLine-Client/Assets/Scripts/Object/BreakableObject.cs
+++ TankLine-Client/Assets/Scripts/Object/BreakableObject.cs
@@ -1,21 +1,32 @@
 using FishNet.Object;
 using FishNet.Object.Synchronizing;
 using UnityEngine;
+using UnityEngine.Formats.Alembic.Importer;
+using System.Collections;
 
 //Object that breaks after a certain number of lives, and changes texture/material with each life
 public class BreakableObject : NetworkBehaviour
 {
     private readonly SyncVar<int> health = new(new SyncTypeSettings(1f)); // Number of hits required to break the object
 
-    public int nbStartingLife = 3;
+    private AlembicStreamPlayer alembicPlayer;
 
-    public Material[] textures; // Texture (public Texture[]) or Material (public Material[]) table to represent the number of lives remaining
+    public int nbStartingLife = 3;
+    public float speed = 1f;
+
+    public float damage_time_1 = 0f;
+    public float damage_time_2 = 0f;
+
     private Renderer objRenderer;
     private Rigidbody rb;
 
     private void Awake()
     {
         objRenderer = GetComponent<Renderer>();
+        alembicPlayer = GetComponent<AlembicStreamPlayer>();
+        alembicPlayer.CurrentTime = 0;
+        alembicPlayer.enabled = false;
+
         if (objRenderer == null)
         {
             Debug.LogError("Renderer not found on " + gameObject.name);
@@ -35,7 +46,6 @@ public class BreakableObject : NetworkBehaviour
             rb.constraints = RigidbodyConstraints.FreezeAll;
         }
 
-        UpdateVisualState(health.Value, health.Value, true); // Apply the correct texture at the start
     }
 
     public void TakeDamage() {
@@ -44,23 +54,62 @@ public class BreakableObject : NetworkBehaviour
 
     public void OnHealthChange(int previousHealth, int nextHealth, bool asServer)
     {
-        if (nextHealth == 0)
+        if (nextHealth == 2)
         {
-            if (base.IsServerInitialized)
-                Despawn(gameObject);
+            StartCoroutine(PerformAnimationPart1());
         }
-
-        UpdateVisualState(previousHealth, nextHealth, true);
+        else if (nextHealth == 1)
+        {
+            StartCoroutine(PerformAnimationPart2());
+        }
+        else if (nextHealth == 0)
+        {
+            StartCoroutine(PerformAnimationPart3());
+        }
     }
 
-    void UpdateVisualState(int oldHealth, int newHealth, bool asServer)
+    private IEnumerator PerformAnimationPart1()
     {
-        if (newHealth > 0 && newHealth <= textures.Length)
+        alembicPlayer.enabled = true;
+        while(alembicPlayer.CurrentTime < (damage_time_1 / speed))
         {
-            // objRenderer.material.mainTexture = textures[newHealth - 1]; // Change the texture
-            objRenderer.material = textures[newHealth - 1]; // Change the material
-
+            if (alembicPlayer != null)
+            {
+                alembicPlayer.CurrentTime += Time.deltaTime*speed;
+            }
+            yield return null;
         }
+        alembicPlayer.enabled = false;
+    }
+
+    private IEnumerator PerformAnimationPart2()
+    {
+        alembicPlayer.enabled = true;
+        while(alembicPlayer.CurrentTime < (damage_time_2 / speed))
+        {
+            if (alembicPlayer != null)
+            {
+                alembicPlayer.CurrentTime += Time.deltaTime*speed;
+            }
+            yield return null;
+        }
+        alembicPlayer.enabled = false;
+    }
+
+    private IEnumerator PerformAnimationPart3()
+    {
+        alembicPlayer.enabled = true;
+        while(alembicPlayer.CurrentTime < (alembicPlayer.Duration / speed))
+        {
+            if (alembicPlayer != null)
+            {
+                alembicPlayer.CurrentTime += Time.deltaTime*speed;
+            }
+            yield return null;
+        }
+        alembicPlayer.enabled = false;
+        if (base.IsServerInitialized)
+            Despawn(gameObject);
     }
 
 }
