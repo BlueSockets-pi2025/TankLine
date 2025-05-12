f288f4bd7cda031782ecd42ca9162616bcabb55b
Ma3ll3 <falch.maelle@gmail.com>
2025-05-12 17:17:53 +0200

    final settings

--- TankLine-Client/Assets/Scripts/Menus/RebindMenu.cs
+++ TankLine-Client/Assets/Scripts/Menus/RebindMenu.cs
@@ -9,16 +9,11 @@ public class RebindMenu : MonoBehaviour
     public InputActionReference MoveRef, ShootRef;
     private void OnEnable()
     {
-        Debug.Log("OnEnable rebindMenu");
-        // MoveRef.action.DeactivateInput();
-        action.Disable();
-        // MoveRef.action.Disable();
+        MoveRef.action.Disable();
         ShootRef.action.Disable();
-
     }
     private void OnDisable()
     {
-        Debug.Log("OnDisable rebindMenu");
         MoveRef.action.Enable();
         ShootRef.action.Enable();
     }
