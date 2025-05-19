0ec976d8f6edaa7ccbae282ed42b7e92c3a758c0
MSI <rayen.zaied@etu.unistra.fr>
2025-05-19 21:01:26 +0200

    edit visibiliy for pc

--- TankLine-Client/Assets/Scripts/Menus/TogglePasswordVisibility.cs
+++ TankLine-Client/Assets/Scripts/Menus/TogglePasswordVisibility.cs
@@ -20,6 +20,7 @@ public class TogglePasswordVisibility : MonoBehaviour
 
     void TogglePassword()
     {
+        #if UNITY_ANDROID
         isPasswordVisible = !isPasswordVisible;
 
         passwordInput.contentType = isPasswordVisible ? 
@@ -28,14 +29,17 @@ public class TogglePasswordVisibility : MonoBehaviour
 
         passwordInput.ForceLabelUpdate();
         UpdateEyeIcon();
+        #endif
     }
 
     void UpdateEyeIcon()
     {
+        #if UNITY_ANDROID
         if (eyeImage != null)
         {
             // Set the texture based on password visibility
             eyeImage.texture = isPasswordVisible ? eyeOpenTexture : eyeClosedTexture;
         }
+        #endif
     }
 }
